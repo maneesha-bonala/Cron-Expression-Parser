4bd1c2755e9f819b70e066bfc550b93339260c3e
maneesha-bonala <143600593+maneesha-bonala@users.noreply.github.com>
2023-08-30 16:12:09 +0530

    cron expression parser

---
+++ src/main/java/parser/fieldparser/HourFieldParser.java
@@ -0,0 +1,13 @@
+package parser.fieldparser;
+
+public class HourFieldParser extends FieldParser {
+    @Override
+    protected int getMin() {
+        return 0;
+    }
+
+    @Override
+    protected int getMax() {
+        return 23;
+    }
+}
---
+++ src/main/java/parser/fieldparser/MinuteFieldParser.java
@@ -0,0 +1,13 @@
+package parser.fieldparser;
+
+public class MinuteFieldParser extends FieldParser {
+    @Override
+    protected int getMin() {
+        return 0;
+    }
+
+    @Override
+    protected int getMax() {
+        return 59;
+    }
+}
